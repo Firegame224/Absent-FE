508e156a96f7d272245af0ec3117b44d1b91da80
Firegame224 <xg474627@gmail.com>
2025-06-13 00:39:06 +0700

    Initial commit⚡

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+
+const value = ref('')
+const action = useAuthStore()
+
+const checkedName = ref([])
+const array = [
+  {
+    name: 'Fiky',
+    kelas: 10,
+  },
+  {
+    name: 'Al',
+    kelas: 12,
+  },
+  {
+    name: 'Rasya',
+    kelas: 11,
+  },
+]
+</script>
+
+<template>
+  <form @submit.prevent="action.signIn(value)">
+    <input type="text" v-model="value" />
+    <p>{{ value }}</p>
+    <button class="w-full p-5 bg-red-500 rounded-md font-semibold text-white" type="submit">
+      Submit
+    </button>
+  </form>
+
+  <div v-for="(item, index) in array" :key="index" class="flex items-center w-full">
+    <p class="text-red-600 font-bold">{{ item.name }}</p>
+    <p>{{ item.kelas }}</p>
+  </div>
+  <input type="text" >
+  <div>Names :{{ checkedName }}</div>
+
+  <select v-model="checkedName" multiple>
+    <option>Fiky</option>
+    <option>Ganteng</option>
+    <option>Banget</option>
+    <option>Hehe</option>
+  </select>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
